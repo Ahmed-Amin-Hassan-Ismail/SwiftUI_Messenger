ca70ed7a841c35ef47f43f6f32e022e0daed3968
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-09-11 17:35:21 +0300

    initial commit

---
+++ MessengerApp/MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Ahmed Amin on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
